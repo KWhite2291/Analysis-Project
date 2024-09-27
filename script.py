3bd0cfc316ed1464d4869a2098a9e1446ed420e3
KWhite2291 <124069458+KWhite2291@users.noreply.github.com>
2024-09-26 22:30:11 -0500

    Main.py

---
+++ script.py
@@ -0,0 +1,12 @@
+# creating variable mom and age
+name = "Kieria"
+age = 25 
+
+#Displaying the sentence with variables
+print(f"My name is Kieria and I am 32 years old.")
+
+#modifying the age variable
+age = age + 10 # or "age +=10"
+
+#Displaying the updated sentence with the variables 
+print(f"my name is Kieria and I am now 42 years old.")
