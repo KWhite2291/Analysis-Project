a8cd48aca0e4387546d246d2acf5c520bca9e380
KWhite2291 <kieriaholmes3@gmail.com>
2025-03-06 15:51:09 -0600

    Update script.py

--- script.py
+++ script.py
@@ -1,74 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 
-def price_including_tax(soup: BeautifulSoup): 
-    elements = soup.select(".price_color")
-    first_element = elements[0]
-    price = first_element.string
-    if price is None: 
-        price = ""
+def extract_description(soup: BeautifulSoup):
+  # you could also use the select method but it would return a list (no need because ids are unique)
+  div_tag = soup.find(id="product_description")  # find by id
+  paragraph_tag = div_tag.find_next("p")  # find sibling paragraph (it's at the same level as div tag)
+  description = paragraph_tag.get_text(strip=True)  # get human readable text (strip=True => remove new lines or whitespace at start and end)
+  return description
 
-    return price 
+def extract_value(soup: BeautifulSoup, selector: str):
+    elements = soup.select(selector)
+    first_element = elements[0]
+    text = first_element.get_text(strip=True)  # get human readable text
+    return text
+
+def extract_value(soup: BeautifulSoup):
 
 url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-
-
-def is_quantity_available(soup: BeautifulSoup): 
-    elements = soup.select(".instock.availability")
-    first_element = elements[0]
-    quantity_available = first_element
-    if quantity_available is None: 
-        quantity_available = ""
-
-    return  quantity_available
-
-def product_description(soup: BeautifulSoup): 
-    product = [p.text for p in soup.select("p") if "It's hard to imagine " in p.text]
-    # first_element = elements[0]
-    # product = first_element
-    # if product is None: 
-    #     product = ""
-
-    return  product
-
-
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-# product_page_url
 
+title = extract_value(soup,".col-sm-6.product_main > h1")
+price = extract_value(soup, ".price_color")
+quantity_available = extract_value(soup, ".instock.availability")
+description = extract_description(soup)
 
-
-# universal_product_code (upc)
-table = soup.select("table")
-td = soup.select("td")
-print("td",td)
-#print("table",table)
-# book_title
-
-
-titles = soup.select("div.col-sm-6.product_main > h1")
-print(titles) #content_inner > article > div.row > div.col-sm-6.product_main > h1
-# price_including_tax
-price = price_including_tax(soup)
-quantity_available = is_quantity_available(soup)
-product = product_description (soup)
+print(title)
 print(price)
 print(quantity_available)
-print(product)
-
-# identify the variable and define it
-# 1 price
-# 2 assign the price a value hardcoding a literal
-# 3 price = 51.77
-# print(price) 
-# grab the body and assign it to price
-# price = soup.select("body")
-# price_excluding_tax
-# quantity_available
+print(description)
 
 
-# product_description
-# category
-# review_rating
-# image_url
+
+
 
